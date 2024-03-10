14779badee5fb7ab2c738eb46eb6db6574d2ed70
Raymond Deng <1107450@gmail.com>
2024-03-09 20:22:18 -0800

    ocaml sarsa vfa

--- dawn_ocaml/lib/vf.ml
+++ dawn_ocaml/lib/vf.ml
@@ -1,5 +1,8 @@
 (* RL: Parametric Value Function Approximation *)
 
+
+(* TD with Function Approximation *)
+
 let linear_fa state theta =
   List.fold_left2 (+.) 0.0 state theta
 
@@ -20,3 +23,37 @@ let TD_VFA env policy gamma theta_0 num_episodes alpha =
       aux (aux_inner theta state) (episode_count + 1)
   in
   aux theta_0 0
+
+(* SARSA with Function Approximation *)
+
+type state = int array
+type action = int
+
+let q_approx weights state action =
+  Array.fold_left2 (+.) 0.0 weights (featurize state action)
+
+let epsilon_greedy q epsilon state =
+  if Random.float 1.0 < epsilon then
+    List.nth (List.permute (possible_actions state)) 0
+  else
+    fst @@ List.max (List.map (fun a -> (a, q state a)) (possible_actions state))
+
+let sarsa_update weights state action reward next_state next_action gamma alpha =
+  let q_cur = q_approx weights state action in
+  let q_next = q_approx weights next_state next_action in
+  let td_error = reward +. gamma *. q_next -. q_cur in
+  Array.map2 (fun w f -> w +. alpha *. td_error *. f) weights (featurize state action)
+
+let sarsa env max_episodes gamma alpha epsilon =
+  let weights = Array.make (num_features env) 0.0 in
+  for episode = 1 to max_episodes do
+    let rec episode_loop state =
+      let action = epsilon_greedy (q_approx weights) epsilon state in
+      let next_state, reward = env_step env state action in
+      let next_action = epsilon_greedy (q_approx weights) epsilon next_state in
+      weights <- sarsa_update weights state action reward next_state next_action gamma alpha;
+      if terminated env next_state then () else episode_loop next_state
+    in
+    episode_loop (env_reset env)
+  done;
+  weights
