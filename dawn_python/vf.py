1c030788e676f9ccd2a4971d38bcdc7aab52d944
Raymond Deng <1107450@gmail.com>
2024-03-10 23:39:39 -0700

    python q-learning vfa

--- dawn_python/vf.py
+++ dawn_python/vf.py
@@ -56,3 +56,29 @@ def sarsa_update(weights: dict[tuple[State, Action], float], state: State, actio
 
 def featurize(state: State, action: Action) -> list[tuple[int, float]]:
     return [(0, 1.0)]
+
+
+
+
+# Q-learning with Function Approximation
+def q_learning_func_approx(gamma, episodes, target_func, observe_func, env):
+    def loop(theta, s):
+        a = choose_action(theta, s)
+        s_prime, r = observe_func(s, a)
+        target = r + gamma * target_func(theta, s_prime)
+        theta_prime = update_weights(theta, s, a, target)
+        if env.terminated(s_prime):
+            return theta_prime
+        else:
+            return loop(theta_prime, s_prime)
+
+    init_theta = initialize_weights()
+
+    def episode(theta, i):
+        if i == 0:
+            return theta
+        s0 = env.reset()
+        theta_prime = loop(theta, s0)
+        return episode(theta_prime, i - 1)
+
+    return episode(init_theta, episodes)
