6cc9061c117f3121abd9db0d41b2946fd0b8dbea
Raymond Deng <1107450@gmail.com>
2024-03-23 23:36:06 -0700

    go gcounter

--- dawn_go/crdt.go
+++ dawn_go/crdt.go
@@ -63,3 +63,58 @@ func (c *Counter) Downstream() []Operation {
 func (c *Counter) Update(op Operation) *Counter {
     return &Counter{c.value, append(c.ops, op)}
 }
+
+// State based increment-only counter
+type GCounter []int
+
+func NewGCounter(size int) GCounter {
+    return make(GCounter, size)
+}
+
+func GCounterOperations() (func(GCounter, int) error, func(GCounter, int) (int, error), func(GCounter, GCounter) (int, error), func(GCounter, GCounter) (GCounter, error)) {
+    update := func(c GCounter, i int) error {
+        if i < 0 || i >= len(c) {
+            return errors.New("Index out of bounds")
+        }
+        c[i]++
+        return nil
+    }
+
+    query := func(c GCounter, i int) (int, error) {
+        if i < 0 || i >= len(c) {
+            return 0, errors.New("Index out of bounds")
+        }
+        return c[i], nil
+    }
+
+    compare := func(c1, c2 GCounter) (int, error) {
+        if len(c1) != len(c2) {
+            return 0, errors.New("Vectors have different lengths")
+        }
+        for i := range c1 {
+            if c1[i] < c2[i] {
+                return -1, nil
+            } else if c1[i] > c2[i] {
+                return 1, nil
+            }
+        }
+        return 0, nil
+    }
+
+    merge := func(c1, c2 GCounter) (GCounter, error) {
+        if len(c1) != len(c2) {
+            return nil, errors.New("Vectors have different lengths")
+        }
+        c := make(GCounter, len(c1))
+        for i := range c1 {
+            if c1[i] > c2[i] {
+                c[i] = c1[i]
+            } else {
+                c[i] = c2[i]
+            }
+        }
+        return c, nil
+    }
+
+    return update, query, compare, merge
+}
