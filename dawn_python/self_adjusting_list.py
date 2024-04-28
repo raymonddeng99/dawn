935bb64d07f0a52869a05322c6614de4147ecdb8
Raymond Deng <1107450@gmail.com>
2024-04-27 23:49:15 -0700

    python self adjusting list

---
+++ dawn_python/self_adjusting_list.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data):
+        self.prev = None
+        self.next = None
+        self.data = data
+
+class SelfAdjustingList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.finger = None
+
+    def insert_front(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.finger = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            self.finger = new_node
+
+    def traverse_from_finger(self, target):
+        node = self.finger
+        while node:
+            if node.data == target:
+                return node
+            node = node.next
+        self.finger = self.tail
+        return None
+
+    def find(self, target):
+        if self.finger:
+            found = self.traverse_from_finger(target)
+            if found:
+                return found
+        if self.head:
+            return self.traverse_from_finger(target)
+        return None
