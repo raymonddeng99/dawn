0367b0e18e87a36ebeb51589eed99faa984a89ad
Raymond Deng <1107450@gmail.com>
2024-04-28 23:18:00 -0700

    c++ const list

--- dawn_cpp/self_adjusting_list.cpp
+++ dawn_cpp/self_adjusting_list.cpp
@@ -8,6 +8,7 @@ struct Node {
     T data;
 };
 
+// Sleator-Tarjan one-finger model
 template <typename T>
 class SelfAdjustingList {
 private:
@@ -33,6 +34,68 @@ private:
 
 public:
     SelfAdjustingList() : head(nullptr), tail(nullptr), finger(nullptr) {}
+
+    void insertFront(const T& data) {
+        auto newNode = std::make_shared<Node<T>>();
+        newNode->data = data;
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+            finger = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+            finger = newNode;
+        }
+    }
+
+    std::shared_ptr<Node<T>> find(const T& target) {
+        if (finger == nullptr) {
+            if (head == nullptr) {
+                return nullptr;
+            }
+            return traverseFromFinger(target, head);
+        }
+        auto found = traverseFromFinger(target, finger);
+        if (found == nullptr) {
+            if (head == nullptr) {
+                return nullptr;
+            }
+            return traverseFromFinger(target, head);
+        }
+        return found;
+    }
+};
+
+
+
+// Constant finger model
+template <typename T>
+class ConstList {
+private:
+    std::shared_ptr<Node<T>> head;
+    std::shared_ptr<Node<T>> tail;
+    std::shared_ptr<Node<T>> finger;
+
+    std::shared_ptr<Node<T>> traverseFromFinger(const T& target, const std::shared_ptr<Node<T>>& node) {
+        if (node->data == target) {
+            return node;
+        }
+        if (node->next == nullptr) {
+            finger = tail;
+            return nullptr;
+        }
+        auto found = traverseFromFinger(target, node->next);
+        if (found != nullptr) {
+            return found;
+        }
+        finger = node;
+        return node;
+    }
+
+public:
+    ConstList() : head(nullptr), tail(nullptr), finger(nullptr) {}
 
     void insertFront(const T& data) {
         auto newNode = std::make_shared<Node<T>>();
