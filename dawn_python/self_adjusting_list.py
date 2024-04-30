dbcc5e1e3365c8960c777b86f44309c1efa123b9
Raymond Deng <1107450@gmail.com>
2024-04-29 23:29:09 -0700

    python obnr list

--- dawn_python/self_adjusting_list.py
+++ dawn_python/self_adjusting_list.py
@@ -78,3 +78,58 @@ class ConstList:
         if self.head:
             return self.traverse_from_finger(target)
         return None
+
+
+# Order by Next Request strategy
+class ONBRList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def remove_head(self):
+        if self.head is None:
+            return None
+        value = self.head.value
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+        return value
+
+    def access(self, value):
+        current = self.head
+        while current is not None:
+            if current.value == value:
+                if current != self.head:
+                    self._remove(current)
+                    self._move_to_front(current)
+                return
+            current = current.next
+
+    def _remove(self, node):
+        if node.prev is not None:
+            node.prev.next = node.next
+        else:
+            self.head = node.next
+        if node.next is not None:
+            node.next.prev = node.prev
+        else:
+            self.tail = node.prev
+
+    def _move_to_front(self, node):
+        node.next = self.head
+        self.head.prev = node
+        self.head = node
+        node.prev = None
