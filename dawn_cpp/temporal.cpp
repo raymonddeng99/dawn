54e70be5dd768d7a8ded5e74c0623baac22e59f7
Raymond Deng <1107450@gmail.com>
2024-05-02 23:43:24 -0700

    c++ temporal dequeue

---
+++ dawn_cpp/temporal.cpp
@@ -0,0 +1,133 @@
+#include <vector>
+#include <variant>
+#include <optional>
+
+template <typename T>
+class Deque {
+private:
+    std::variant<std::monostate, T, std::pair<T, T>, std::tuple<T, T, T>, std::tuple<T, T, T, T>> node;
+
+    std::vector<std::variant<std::monostate, T, std::pair<T, T>, std::tuple<T, T, T>, std::tuple<T, T, T, T>>> front;
+    std::vector<std::variant<std::monostate, T, std::pair<T, T>, std::tuple<T, T, T>, std::tuple<T, T, T, T>>> back;
+
+    void addFrontHelper(const T& x) {
+        std::vector<std::variant<std::monostate, T, std::pair<T, T>, std::tuple<T, T, T>, std::tuple<T, T, T, T>>> newFront;
+        for (const auto& n : front) {
+            if (std::holds_alternative<std::monostate>(n)) {
+                newFront.emplace_back(x);
+            } else if (auto* p = std::get_if<T>(&n)) {
+                newFront.emplace_back(std::make_tuple(*p, *p, *p, x));
+                newFront.emplace_back(*p);
+            } else if (auto* p = std::get_if<std::pair<T, T>>(&n)) {
+                newFront.emplace_back(std::make_tuple(p->first, p->first, p->second));
+                newFront.emplace_back(std::make_tuple(p->second, x, x, x));
+            } else if (auto* p = std::get_if<std::tuple<T, T, T>>(&n)) {
+                newFront.emplace_back(std::make_pair(std::get<0>(*p), std::get<1>(*p)));
+                newFront.emplace_back(std::make_pair(std::get<1>(*p), std::get<2>(*p)));
+                newFront.emplace_back(std::make_tuple(std::get<2>(*p), std::get<2>(*p), std::get<2>(*p), std::get<2>(*p)));
+                newFront.emplace_back(x);
+            } else if (auto* p = std::get_if<std::tuple<T, T, T, T>>(&n)) {
+                newFront.emplace_back(std::get<0>(*p));
+                newFront.emplace_back(std::get<1>(*p));
+                newFront.emplace_back(std::get<2>(*p));
+                newFront.emplace_back(std::get<3>(*p));
+                newFront.emplace_back(x);
+            }
+        }
+        newFront.emplace_back(x);
+        front = std::move(newFront);
+    }
+
+    void addBackHelper(const T& x) {
+        std::vector<std::variant<std::monostate, T, std::pair<T, T>, std::tuple<T, T, T>, std::tuple<T, T, T, T>>> newBack;
+        for (const auto& n : back) {
+            if (std::holds_alternative<std::monostate>(n)) {
+                newBack.emplace_back(x);
+            } else if (auto* p = std::get_if<T>(&n)) {
+                newBack.emplace_back(*p);
+                newBack.emplace_back(std::make_tuple(*p, *p, *p, x));
+            } else if (auto* p = std::get_if<std::pair<T, T>>(&n)) {
+                newBack.emplace_back(std::make_tuple(p->first, p->second, p->second));
+                newBack.emplace_back(std::make_tuple(p->first, p->first, p->first, x));
+            } else if (auto* p = std::get_if<std::tuple<T, T, T>>(&n)) {
+                newBack.emplace_back(std::make_pair(std::get<0>(*p), std::get<1>(*p)));
+                newBack.emplace_back(std::make_pair(std::get<2>(*p), std::get<2>(*p)));
+                newBack.emplace_back(std::make_tuple(std::get<1>(*p), std::get<1>(*p), std::get<1>(*p), std::get<1>(*p)));
+                newBack.emplace_back(x);
+            } else if (auto* p = std::get_if<std::tuple<T, T, T, T>>(&n)) {
+                newBack.emplace_back(std::get<0>(*p));
+                newBack.emplace_back(std::get<1>(*p));
+                newBack.emplace_back(std::get<2>(*p));
+                newBack.emplace_back(std::get<3>(*p));
+                newBack.emplace_back(x);
+            }
+        }
+        newBack.emplace_back(x);
+        back = std::move(newBack);
+    }
+
+public:
+    Deque() : front(), back() {}
+
+    bool isEmpty() const {
+        return front.empty() && back.empty();
+    }
+
+    void addFront(const T& x) {
+        addFrontHelper(x);
+    }
+
+    void addBack(const T& x) {
+        addBackHelper(x);
+    }
+
+    std::optional<T> takeFront() {
+        if (front.empty()) {
+            return std::nullopt;
+        }
+        T x;
+        if (std::holds_alternative<T>(front.front())) {
+            x = std::get<T>(front.front());
+            front.erase(front.begin());
+        } else if (auto* p = std::get_if<std::pair<T, T>>(&front.front())) {
+            x = p->first;
+            front.front() = p->second;
+        } else if (auto* p = std::get_if<std::tuple<T, T, T>>(&front.front())) {
+            x = std::get<0>(*p);
+            front.front() = std::make_pair(std::get<1>(*p), std::get<2>(*p));
+        } else if (auto* p = std::get_if<std::tuple<T, T, T, T>>(&front.front())) {
+            x = std::get<0>(*p);
+            front.front() = std::make_tuple(std::get<1>(*p), std::get<2>(*p), std::get<3>(*p));
+        }
+        if (back.empty()) {
+            back = std::move(front);
+            front.clear();
+        } else {
+            std::vector<std::variant<std::monostate, T, std::pair<T, T>, std::tuple<T, T, T>, std::tuple<T, T, T, T>>> temp = std::move(back);
+            back = std::move(front);
+            front = std::move(temp);
+        }
+        return x;
+    }
+
+    std::optional<T> takeBack() {
+        if (back.empty()) {
+            return std::nullopt;
+        }
+        T x;
+        if (std::holds_alternative<T>(back.back())) {
+            x = std::get<T>(back.back());
+            back.pop_back();
+        } else if (auto* p = std::get_if<std::pair<T, T>>(&back.back())) {
+            x = p->second;
+            back.back() = p->first;
+        } else if (auto* p = std::get_if<std::tuple<T, T, T>>(&back.back())) {
+            x = std::get<2>(*p);
+            back.back() = std::make_pair(std::get<0>(*p), std::get<1>(*p));
+        } else if (auto* p = std::get_if<std::tuple<T, T, T, T>>(&back.back())) {
+            x = std::get<3>(*p);
+            back.back() = std::make_tuple(std::get<0>(*p), std::get<1>(*p), std::get<2>(*p));
+        }
+        return x;
+    }
+};
