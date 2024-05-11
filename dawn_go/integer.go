d4c0d7384f4c3000455e1d305e9112c3873cc8d1
Raymond Deng <1107450@gmail.com>
2024-05-10 23:39:05 -0700

    go sig sort

---
+++ dawn_go/integer.go
@@ -0,0 +1,33 @@
+package signatureSort
+
+func signatureSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := signatureSort(arr[:mid])
+	right := signatureSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, 0, len(left)+len(right))
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+
+	return result
+}
