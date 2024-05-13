4ff86c12bfa8417b8091d14f312449161f0bfe2c
Raymond Deng <1107450@gmail.com>
2024-05-12 23:41:56 -0700

    python succinct rank and select

--- dawn_python/succinct.py
+++ dawn_python/succinct.py
@@ -25,3 +25,48 @@ class CompactSuffixArray:
 
     def compact_suffix_array(self):
         return self.indices
+
+
+# Succinct static data structures, Jacobsen '89
+class BitVector:
+    k = 6
+    
+    def __init__(self, bits):
+        self.bits = bits
+        n = len(bits)
+        self.rank_table = [0] * (n + 1)
+        self.select_table = [-1] * (n + 1)
+
+        for i in range(n + 1):
+            if i % (1 << self.k) == 0:
+                self.rank_table[i] = 0
+                self.select_table[i] = -1
+            else:
+                prev = i - (i % (1 << self.k))
+                self.rank_table[i] = self.rank_table[prev] + self.rank_naive(bits, i) - self.rank_naive(bits, prev)
+                self.select_table[i] = self.select_table[prev] + self.select_naive(bits, i) - self.select_naive(bits, prev)
+
+    def rank_naive(self, bits, i):
+        return sum(1 for b in bits[:i + 1] if b)
+
+    def select_naive(self, bits, i):
+        cnt, j = 0, 0
+        for j in range(len(bits)):
+            if bits[j]:
+                cnt += 1
+                if cnt == i + 1:
+                    return j
+        return -1
+
+    def rank(self, i):
+        return self.rank_table[i]
+
+    def select(self, i):
+        l, r = 0, len(self.bits)
+        while l <= r:
+            m = (l + r) // 2
+            if self.rank(m) < i:
+                l = m + 1
+            else:
+                r = m - 1
+        return l if l < len(self.bits) and self.rank(l) == i else -1
