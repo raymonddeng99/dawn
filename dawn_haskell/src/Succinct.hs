d55470c27ab0d82ad57f615b6a1906351ae94a06
Raymond Deng <1107450@gmail.com>
2024-05-12 23:05:21 -0700

    haskell succinct rank and select

--- dawn_haskell/src/Succinct.hs
+++ dawn_haskell/src/Succinct.hs
@@ -22,3 +22,47 @@ suffixArray text =
       ranks = listArray (0, n-1) [rankSuffix text rankOfChar n i | i <- [0..n-1]]
   in
     map fst $ sortBy (comparing snd) $ zip [0..n-1] (elems ranks)
+
+
+
+-- Succinct static data structures, Jacobsen '89
+module BitVector (BitVector, create, rank, select) where
+
+import Data.Vector (Vector, fromList, (!?), (!))
+import qualified Data.Vector as V
+
+k :: Int
+k = 6
+
+rankNaive :: Vector Bool -> Int -> Int
+rankNaive bits i = sum $ V.map (\b -> if b then 1 else 0) $ V.take (i + 1) bits
+
+selectNaive :: Vector Bool -> Int -> Int
+selectNaive bits i = go 0 0
+  where
+    go j cnt
+      | cnt == i = j
+      | j >= V.length bits = -1
+      | otherwise = go (j + 1) (cnt + if bits ! j then 1 else 0)
+
+create :: [Bool] -> BitVector
+create bits = BitVector bits rankTable selectTable
+  where
+    n = length bits
+    rankTable = V.generate (n + 1) (\i -> if i `mod` (1 `shiftL` k) == 0 then 0 else rankTable V.! (i - (i `mod` (1 `shiftL` k))) + rankNaive (V.fromList bits) i - rankNaive (V.fromList bits) (i - (i `mod` (1 `shiftL` k))))
+    selectTable = V.generate (n + 1) (\i -> if i `mod` (1 `shiftL` k) == 0 then -1 else selectTable V.! (i - (i `mod` (1 `shiftL` k))) + selectNaive (V.fromList bits) i - selectNaive (V.fromList bits) (i - (i `mod` (1 `shiftL` k))))
+
+data BitVector = BitVector (Vector Bool) (Vector Int) (Vector Int)
+
+rank :: BitVector -> Int -> Int
+rank (BitVector _ rankTable _) i = rankTable V.! i
+
+select :: BitVector -> Int -> Int
+select (BitVector bits _ selectTable) i = go 0 (V.length bits)
+  where
+    go l r
+      | l > r = -1
+      | otherwise = let m = l + (r - l) `div` 2 in
+                    if rank (BitVector bits rankTable selectTable) m < i
+                    then go (m + 1) r
+                    else go l (m - 1)
