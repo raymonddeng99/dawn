c7a75f969f41ac25831368a416b763368f1f0a2c
Raymond Deng <1107450@gmail.com>
2024-05-12 23:37:41 -0700

    c++ succinct rank and select

--- dawn_cpp/succinct.cpp
+++ dawn_cpp/succinct.cpp
@@ -2,6 +2,7 @@
 #include <array>
 #include <string>
 #include <vector>
+#include <cmath>
 
 class SuffixArray {
 public:
@@ -40,5 +41,58 @@ private:
             }
         }
         return rankOfChar;
+    }
+};
+
+
+// Succinct static data structures, Jacobsen '89
+class BitVector {
+public:
+    BitVector(const std::vector<bool>& bv) : bits(bv) {
+        int n = bits.size();
+        rank_table.resize(n + 1);
+        select_table.resize(n + 1);
+
+        for (int i = 0; i <= n; i++) {
+            rank_table[i] = (i % (1 << k)) ? rank_table[i - (i % (1 << k))] + rank_naive(i) - rank_naive(i - (i % (1 << k))) : 0;
+            select_table[i] = (i % (1 << k)) ? select_table[i - (i % (1 << k))] + select_naive(i) - select_naive(i - (i % (1 << k))) : -1;
+        }
+    }
+
+    int rank(int i) {
+        return rank_table[i];
+    }
+
+    int select(int i) {
+        int l = 0, r = bits.size();
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (rank(m) < i)
+                l = m + 1;
+            else
+                r = m - 1;
+        }
+        return l;
+    }
+private:
+    std::vector<bool> bits;
+    std::vector<int> rank_table;
+    std::vector<int> select_table;
+    const int k = 6;
+
+    int rank_naive(int i) {
+        int cnt = 0;
+        for (int j = 0; j <= i; j++)
+            if (bits[j])
+                cnt++;
+        return cnt;
+    }
+
+    int select_naive(int i) {
+        int cnt = 0, j;
+        for (j = 0; cnt < i; j++)
+            if (bits[j])
+                cnt++;
+        return (cnt == i) ? j - 1 : -1;
     }
 };
